55d56882c4dfdc34178450752c507506a5989083
fedeparrilla10 <federicoivanpd@gmail.com>
2024-08-06 14:21:28 +0300

    Add /figueiras/ to routes

--- src/data/houses.ts
+++ src/data/houses.ts
@@ -9,7 +9,7 @@ export const homeHouses: House[] = [
     description:
       '¡Bienvenidos a una de nuestras casas rurales en Asturias cerca del mar! Alameda 33 es una casa vacacional completa y rehabilitada en el centro del tranquilo pueblo de Figueras, frente de Ribadeo. Tiene capacidad para 8 huéspedes en 4 habitaciones, cuenta con 3 baños completos, un amplio salón, cocina equipada y un tranquilo patio exterior de 20 m² con un pequeño porche donde poder pasar agradables veladas y que además en invierno puedes cerrar y encender su estufa de pellets. Incluye ropa de cama, toallas y kit de bienvenida. ¡Disfruta tu alquiler vacacional cerca de Ribadeo!',
     registerNumber: 'VV-2060-AS',
-    bookingImg: 'bookingalameda33.png',
+    bookingImg: '/figueiras/bookingalameda33.png',
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ export const homeHouses: House[] = [
     description:
       '¡Ven a nuestra casa rural en Asturias con tu perro! Bienvenidos a Jovellanos, en el pintoresco pueblo costero de la Ría Eo-Ribadeo, Figueras. Esta casa vacacional ofrece la posibilidad de alquilarla como casa completa para 6 personas con 2 baños completos o como apartamento para 3 personas con 1 baño completo. Con ambas opciones la casa incluye un salón con estufa de leña para noches acogedoras, una cocina equipada y un tranquilo patio de 20 m² para momentos especiales. Incluimos ropa de cama, toallas y un kit de bienvenida. ¡Te sentirás como en casa y tu mascota es bienvenida!',
     registerNumber: 'VV-1602-AS',
-    bookingImg: 'bookingjovellanos.png',
+    bookingImg: '/figueiras/bookingjovellanos.png',
   },
   {
     id: 3,
@@ -29,6 +29,6 @@ export const homeHouses: House[] = [
     description:
       '¡Ven a pasar unos días en nuestra casa rural en Asturias! Disfruta de una casa completa en el centro de un pueblo tranquilo, ideal para una escapada. La casa vacacional tiene capacidad para 5 huéspedes en 3 cómodas habitaciones, cuenta con 2 baños completos, un salón con estufa de pellets para las tardes más acogedoras y una cocina totalmente equipada. Dispondrás de ropa de cama, toallas y kit de bienvenida incluidos. ¡Disfruta tu estancia en este hermoso pueblo costero de la Ría Eo-Ribadeo!    ',
     registerNumber: 'VV-1823-AS',
-    bookingImg: 'bookingalameda23.png',
+    bookingImg: '/figueiras/bookingalameda23.png',
   },
 ];
