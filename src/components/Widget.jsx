fc6a627a7582fbe193bcc64b6239b95b07912024
fedeparrilla10 <federicoivanpd@gmail.com>
2024-08-08 16:16:28 +0300

    update

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,49 +1,26 @@
+import React, { useState, useEffect } from 'react';
 
-// import React, { useState, useEffect } from 'react';
+const Widget = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    // Simula una carga de contenido
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Cambia el tiempo según lo necesario
 
-// const Widget = () => {
-//     const [isLoading, setIsLoading] = useState(true);
+    return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta
+  }, []);
 
-//     useEffect(() => {
-//         // Simula una carga de contenido
-//         const timer = setTimeout(() => {
-//             setIsLoading(false);
-//         }, 3000); // Cambia el tiempo según lo necesario
-
-//         return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta
-//     }, []);
-
-//     return (
-//         <div className={`widget ${isLoading ? 'loading' : ''}`}>
-//             {isLoading ? (
-//                 <div className="loader">Cargando...</div>
-//             ) : (
-//                 <div>
-//                     {/* Contenido del widget que se carga dinámicamente */}
-
-
-//                     1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <div className={`widget ${isLoading ? 'loading' : ''}`}>
+      {isLoading ? (
+        <div className="loader">Cargando...</div>
+      ) : (
+        <div>{/* Contenido del widget que se carga dinámicamente */}1</div>
+      )}
+    </div>
+  );
 };
 
 export default Widget;
-
-
