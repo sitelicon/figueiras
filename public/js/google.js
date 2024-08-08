65fc5fee304d8987fb76ca9f9bc1508f647e615f
Damian <damian@sitelicon.com>
2024-08-08 15:02:58 -0300

    avances

---
+++ public/js/google.js
@@ -0,0 +1,23 @@
+
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-2NHBQ26PMH');
+  gtag('config', 'AW-10858659939');
+
+
+
+
+
+
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-221395340-1');
+  
--- src/data/news.ts
+++ src/data/news.ts
@@ -3,34 +3,46 @@ import type { News } from './types';
 export const lastNews: News[] = [
   {
     id: 1,
-    img: '/figueiras/news/asturias.jpg',
+    img: '/figueiras/news/foto1.webp',
     title:
       'Qué ver en Asturias; el lugar donde todos quieren pasar sus vacaciones',
+    link: 'https://www.telva.com/estilo-vida/viajes/2021/08/15/6114fbf101a2f11cb88b4585.html?fbclid=IwAR3awtGLTZ3reb_rj_i0C2A6ozTz2VZgj1QJRjWb2JrOXp7ljkQRhHKXJgI',
   },
   {
     id: 2,
-    img: '/figueiras/news/occidente.png',
+    img: '/figueiras/news/foto2.webp',
+    link: 'https://www.elcomercio.es/asturias/occidente/costa-occidente-reconocida-mejor-destino-turistico-espana-20190605165005-nt.html#vca=fixed-btn&vso=rrss&vmc=wh&vli=Occidente',
     title:
       'La costa del Occidente, reconocida como mejor destino turístico de España',
   },
   {
     id: 3,
-    img: '/figueiras/news/ribadeo.jpg',
+    img: '/figueiras/news/foto3.webp',
+    link: 'https://www.youtube.com/watch?v=sIvJleaei38&authuser=0',
     title: 'RIBADEO - Lo que la ría esconde',
   },
   {
     id: 4,
-    img: '/figueiras/news/ribadeo.jpg',
-    title: 'RIBADEO - Lo que la ría esconde',
+    img: '/figueiras/news/foto4.webp',
+    link: 'https://www.lavozdegalicia.es/noticia/amarina/ribadeo/2019/11/03/mejores-mundo-surfeanolas-gigantes-ribadeo/00031572787914907775839.htm',
+    title: '"Los mejores del mundo surfean olas gigantes en Ribadeo"',
   },
   {
     id: 5,
-    img: '/figueiras/news/ribadeo.jpg',
-    title: 'RIBADEO - Lo que la ría esconde',
+    img: '/figueiras/news/foto5.webp',
+    link: 'https://elviajero.elpais.com/elviajero/2019/10/17/actualidad/1571306604_622713.html?fbclid=IwAR1x8AEmFYyDGSx5BYfULSgjXBCo8MOyWk0PjWHiONBFr8SFxY36T1nkaMw',
+    title: '"Catorce sorpresas asturianas"',
   },
   {
     id: 6,
-    img: '/figueiras/news/ribadeo.jpg',
-    title: 'RIBADEO - Lo que la ría esconde',
+    img: '/figueiras/news/foto6.webp',
+    link: 'https://www.traveler.es/gastronomia/articulos/los-seis-platos-que-hay-que-comer-en-asturias/5779',
+    title: '"Seis platos que hay que comer en Asturias"',
+  },
+  {
+    id: 7,
+    img: '/figueiras/news/foto7.webp',
+    link: 'https://www.escapadarural.com/que-hacer/castropol/que-ver',
+    title: '"Qué ver en Castropol"',
   },
 ];
