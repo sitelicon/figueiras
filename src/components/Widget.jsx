dbb2fc78143a3b251a8ea1c42fe5f75760ae1c6a
fedeparrilla10 <federicoivanpd@gmail.com>
2024-08-08 16:02:50 +0300

    update

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,29 +1,29 @@
 
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
 
-const Widget = () => {
-    const [isLoading, setIsLoading] = useState(true);
+// const Widget = () => {
+//     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        // Simula una carga de contenido
-        const timer = setTimeout(() => {
-            setIsLoading(false);
-        }, 3000); // Cambia el tiempo según lo necesario
+//     useEffect(() => {
+//         // Simula una carga de contenido
+//         const timer = setTimeout(() => {
+//             setIsLoading(false);
+//         }, 3000); // Cambia el tiempo según lo necesario
 
-        return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta
-    }, []);
+//         return () => clearTimeout(timer); // Limpiar el temporizador si el componente se desmonta
+//     }, []);
 
-    return (
-        <div className={`widget ${isLoading ? 'loading' : ''}`}>
-            {isLoading ? (
-                <div className="loader">Cargando...</div>
-            ) : (
-                <div>
-                    {/* Contenido del widget que se carga dinámicamente */}
+//     return (
+//         <div className={`widget ${isLoading ? 'loading' : ''}`}>
+//             {isLoading ? (
+//                 <div className="loader">Cargando...</div>
+//             ) : (
+//                 <div>
+//                     {/* Contenido del widget que se carga dinámicamente */}
 
 
-                    1
+//                     1
 
 
 
